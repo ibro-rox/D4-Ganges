62d561247ae3ca5a9d9e1c6cabfc3e604e22e222
bdr1g15 <bdr1g15@ecs.soton.ac.uk>
2017-02-26 22:22:19 +0000

    IR code for Il Matto with a serial interface
    Code for Sharp 2y0a21 IR sensor module. Uses ADC on pin A0. Scaling
    factor of the voltage value found to be 24 by calibrating the sensor at
    a known distance. Output displayed in PuTTy.

---
+++ IR/IR.c
@@ -0,0 +1,60 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <math.h>
+#include "debug.h"
+
+// avr-gcc -mmcu=atmega644p -DF_CPU=12000000 -Wall -Os -Wl,-u,vfprintf -lprintf_flt -lm IR.c -o IR.elf
+// avr-objcopy -O ihex IR.elf IR.hex
+// avrdude -c usbasp -p m644p -U flash:w:IR.hex
+
+
+void init_adc(void)
+{
+	ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADEN);		
+	ADMUX |= _BV(REFS0);	
+}
+
+uint16_t read_adc(void)
+{	
+	ADCSRA |= _BV(ADSC);
+	while(ADCSRA & _BV(ADSC));
+		
+	return ADC;
+}
+
+double to_distance(uint16_t adc_value)
+{
+	double distance,volts;
+	
+	volts = (adc_value*3.3)/1024; 	
+
+	distance = 24/volts;
+
+	return distance;
+}
+
+
+
+int main(void)
+{
+	uint16_t result;
+	double voltage;
+	
+	init_debug_uart0();
+	
+		
+	init_adc();
+	
+	for (;;) 
+	{							
+		result = read_adc();
+		
+		voltage = to_distance(result);
+		printf("%.6f\n",voltage);
+		
+		result = 0x0000;
+		voltage = 0;
+		
+		_delay_ms(1000);
+	}
+}
