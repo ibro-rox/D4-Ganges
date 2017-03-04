077f91442e460588d1d34190b9e9edaa0635e37f
bdr1g15 <bdr1g15@ecs.soton.ac.uk>
2017-03-04 15:46:55 +0000

    Il matto test code, transmit packet of data on external pin interrupt

--- Servo/onboard_serial_test.c
+++ Servo/onboard_serial_test.c
@@ -1,29 +1,78 @@
 #include <avr/io.h>
 #include <util/delay.h>
+
+#include <avr/interrupt.h>
+
 #include "debug.h"
+
 #define BAUD 9600
 #include <util/setbaud.h>
 
+volatile uint8_t serial_interrupt_flag = 0;
+
+ISR(INT0_vect)
+{
+	//printf("Hello from ISR");
+	serial_interrupt_flag = 1;
+}
+
+struct euler_angles {
+	uint16_t throttle;
+	uint16_t yaw;
+	uint16_t pitch;
+	uint16_t roll;
+};
+
+
+
+void send_packet(struct euler_angles *input);
 
 
 
 int main()
 {
-	const uint8_t segments[10] = {0xFC,0x60,0xDA,0xF2,0x66,0xB6,0xBE,0xE0,0xFE,0xF6};
-	uint8_t i;
+	
 	init_debug_uart0();
 	
+	struct euler_angles desired_values;
 	
-	for(;;)
+	desired_values.throttle = 1;
+	desired_values.yaw = 2;
+	desired_values.pitch = 3;
+	desired_values.roll = 4;
+	
+	DDRD |= _BV(PD2);
+	PORTD &= ~_BV(PD2);
+	/* Set to trigger on rising edge */
+	EICRA |= _BV(ISC01) | _BV(ISC00);
+	/* Enable interrupt */
+	EIMSK |= _BV(INT0);
+	sei();	
+	
+	while(1)
 	{
-		for(i=0;i<10;i++)
+		if(serial_interrupt_flag)
+			send_packet(&desired_values);
+		
+		
+		
+		
+		/* for(i=0;i<10;i++)
 		{
 			printf("%d = 0x%x\n",i,segments[i]);
 			_delay_ms(1000);
 			
 		}	
-		 fprintf(stderr,"Count overflow\n\r");
+		 fprintf(stderr,"Count overflow\n\r"); */
 	}
-	
+}
 
+void send_packet(struct euler_angles *input)
+{
+	if((input->throttle <= 1023) && (input->yaw <= 1023) && (input->pitch <= 1023) && (input->roll <= 1023))
+		printf("T%d Y%d P%d R%d\n", input->throttle, input->yaw, input->pitch, input->roll);
+	else 
+		printf("Error\n");
+	
+		serial_interrupt_flag = 0;
 }
