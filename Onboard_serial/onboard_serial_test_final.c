7355beb5c12eb922448bf6c88739cc6f99bf1139
bdr1g15 <bdr1g15@ecs.soton.ac.uk>
2017-03-16 11:53:06 +0000

    New Onboard serial screenshots

--- Onboard_serial/onboard_serial_test_final.c
+++ Onboard_serial/onboard_serial_test_final.c
@@ -23,15 +23,6 @@ struct euler_angles {						//Structure containing TYPR
 	uint16_t yaw;
 	uint16_t pitch;
 	uint16_t roll;
-	uint16_t kp_y;
-	uint16_t ki_y;
-	uint16_t kd_y;
-	uint16_t kp_p;
-	uint16_t ki_p;
-	uint16_t kd_p;
-	uint16_t kp_r;
-	uint16_t ki_r;
-	uint16_t kd_r;
 };
 
 uint8_t is_valid_packet(struct euler_angles *input);
@@ -44,20 +35,11 @@ int main()
 	
 	struct euler_angles desired_values;
 	
-	desired_values.throttle = 700;
+	desired_values.throttle = 512;
 	desired_values.yaw = 554;
 	desired_values.pitch = 747;
-	desired_values.roll = 517;
-	desired_values.kp_y = 101;
-	desired_values.ki_y	= 302;
-	desired_values.kd_y = 599;
-	desired_values.kp_p = 517;
-	desired_values.ki_p	= 911;
-	desired_values.kd_p = 1020;
-	desired_values.kp_r = 23;
-	desired_values.ki_r	= 444;
-	desired_values.kd_r = 777;
-	
+	desired_values.roll = 350;
+		
 	
 	DDRD |= _BV(PD2);			// Set PD2 to output
 	PORTD &= ~_BV(PD2);
@@ -83,11 +65,7 @@ void send_packet(struct euler_angles *input)
 {
 	if(is_valid_packet(input))
 	{
-		printf("t%dy%dh%d\n", input->throttle, input->yaw, input->ki_r);
-		printf("r%d\n", input->roll);
-		printf("b%dd%di%d\n",input->ki_y,input->kp_p,input->kd_r);
-		printf("a%dc%d\n",input->kp_y,input->kd_y);
-		printf("e%df%dg%dh%d\n",input->ki_p,input->kd_p,input->kp_r,input->ki_r);
+		printf("y");//printf("t%dy%dp%dr%d\n", input->throttle, input->yaw, input->pitch, input->roll); //
 	}
 	
 	else 
